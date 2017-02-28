3398db7d936339fa5a28a79bcc3797cc0e8fd775
QUang Truc <quangtruc810@gmail.com>
2017-02-28 18:36:03 +0700

    first

---
+++ app/src/main/java/com/adem/activities/MainActivity.java
@@ -0,0 +1,144 @@
+package com.adem.activities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.adem.R;
+import com.adem.controller.ParseJsonLogin;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //keyHash
+
+        /*try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.adem",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }*/
+
+
+        //faebook
+
+        if (AccessToken.getCurrentAccessToken() == null) {
+            gotoLoginScreem();
+        } else {
+            AccessToken accesstoken = AccessToken.getCurrentAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            Log.d("accesstoken",AccessToken.getCurrentAccessToken().getToken());
+            ParseJsonLogin login = new ParseJsonLogin(AccessToken.getCurrentAccessToken(),this);
+            login.setInfoUser();
+
+
+        }
+
+        //navigation
+
+    }
+
+    private void gotoLoginScreem() {
+        Intent intent= new Intent(this, login_facebook.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_adv_post) {
+            // Handle the camera action
+        } else if (id == R.id.nav_bussines_manage) {
+
+        } else if (id == R.id.nav_auto_post) {
+
+        } else if (id == R.id.nav_management) {
+
+        } else if (id == R.id.nav_invite_friends) {
+
+        } else if (id == R.id.nav_logout) {
+            LoginManager.getInstance().logOut();
+            gotoLoginScreem();
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
